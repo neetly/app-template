9f87ee526aed65404c00720ceb6a997f91a91a36
Hikari Hayashi <rev.hikari@gmail.com>
2020-12-08 18:27:50 +0800

    Initial commit

---
+++ .yarn/plugins/@yarnpkg/plugin-compat.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "@yarnpkg/plugin-compat",
+  factory: () => {
+    return {};
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { render } from "react-dom";
+
+render(
+  <StrictMode>
+    <div />
+  </StrictMode>,
+  document.querySelector("#root"),
+);
